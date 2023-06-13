6113060391c546260455ccb8cdf34e79576f436a
ogirians <ogi.rians3@gmail.com>
2023-06-14 00:05:35 +0700

    halaman index

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,56 +29,15 @@
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />            
       <v-toolbar-title>{{ title }}</v-toolbar-title>
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <v-spacer />      
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
@@ -99,19 +58,19 @@ export default {
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'generate sales contract',
           to: '/'
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
+          title: 'generate lain-lain',
           to: '/inspire'
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Arion Panca Sekawan'
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,232 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+  <v-container>
+    <v-card class="logo" color="primary">
+        <v-card-title class="pt-5 pt-md-0 pb-md-0">
+            <h3 style="color:white" class="mr-5"> 
+              sales contract
+            </h3>
+            <v-text-field
+              class="mt-3"
+              dark
+              value="SC/APS/XXX/bulan/tahun"
+            >
+                
+            </v-text-field>          
         </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
+    </v-card>      
+    <v-card class="logo py-4">   
+      <v-container>
+        <v-row class="mt-5">
+          <v-col class="py-0" cols="12">
+            <v-autocomplete
+              label = "cari nama customer"
+              clearable
+              filled
+              v-if="isAdding == false"
+              dense
             >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
+              <template v-slot:append-outer>
+                <v-slide-x-reverse-transition
+                  mode="out-in"
+                >
+                  <v-icon
+                    :key="`icon-${isAdding}`"
+                    :color="isAdding ? 'success' : 'info'"
+                    @click="isAdding = !isAdding"
+                    v-text="isAdding ? 'mdi-list-box-outline' : 'mdi-plus-circle'"
+                  ></v-icon>
+                </v-slide-x-reverse-transition>
+              </template>      
+            </v-autocomplete>
+            <v-text-field
+              label="nama customer baru"
+              placeholder="nama"
+              filled
+              v-if ="isAdding == true"
+              dense
             >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
+              <template v-slot:append-outer>
+                <v-slide-x-reverse-transition
+                  mode="out-in"
+                >
+                  <v-icon
+                    :key="`icon-${isAdding}`"
+                    :color="isAdding ? 'success' : 'info'"
+                    @click="isAdding = !isAdding"
+                    v-text="isAdding ? 'mdi-list-box-outline' : 'mdi-plus-circle'"
+                  ></v-icon>
+                </v-slide-x-reverse-transition>
+              </template>      
+            </v-text-field>       
+          </v-col>
+          <v-col class="py-0" cols="12" md="6">            
+            <v-text-field
+              label="npwp"              
+              filled              
+              dense
+            >              
+            </v-text-field>                   
+          </v-col>
+          <v-col class="py-0" cols="12" md="6">            
+            <v-text-field
+              label="alamat"              
+              filled    
+              small         
+              dense 
+            >              
+            </v-text-field>                   
+          </v-col>
+        </v-row>
+      </v-container>    
+    </v-card>
+    <v-dialog
+        ref="dialog"
+        v-model="modal"
+        :return-value.sync="date"
+        persistent
+        width="290px"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field            
+            v-model="date"
+            label="tanggal"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+            
+            class="mt-10 col-12 col-md-3"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="date"
+          scrollable          
+        >
+          <v-spacer></v-spacer>
           <v-btn
+            text
             color="primary"
-            nuxt
-            to="/inspire"
+            @click="modal = false"
           >
-            Continue
+            Cancel
           </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+          <v-btn
+            text
+            color="primary"
+            @click="$refs.dialog.save(date)"
+          >
+            OK
+          </v-btn>
+        </v-date-picker>
+    </v-dialog>
+    <v-card color="primary" dark>
+       <v-card-title>
+        List barang
+        <v-spacer></v-spacer>
+        <v-btn small rounded color="success" @click="AddProduct()">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-card-title>
+       
+    </v-card>
+    <v-card class="logo">
+      <v-simple-table        
+        
+      >
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">
+                NO
+              </th>
+              <th class="text-left">
+                JENIS BARANG
+              </th>
+              <th class="text-left">
+                CODE COIL
+              </th>
+              <th class="text-left">
+                QTY (Kg)
+              </th>
+              <th class="text-left">
+                TOTAL (Mtr)
+              </th>
+              <th class="text-left">
+                HARGA
+              </th>
+              <th class="text-left">
+                TOTAL
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="item in products"              
+            >
+              <td>
+                 <input style="width: 20px;" type="text" v-model="item.no"/>                    
+              </td>              
+              <td>
+                 <input style="width: 300px;" type="text" v-model="item.jenis_barang"/>                    
+              </td>              
+              <td>
+                 <input style="width: 100px;" type="text" v-model="item.code_coil"/>                    
+              </td>              
+              <td>
+                 <input style="width: 70px;" type="text" v-model="item.qty"/>                    
+              </td>              
+              <td>
+                 <input style="width: 70px;" type="text" v-model="item.total_mtr"/>                    
+              </td>              
+              <td>
+                 <input style="width: 70px;" type="text" v-model="item.harga"/>                    
+              </td>              
+              <td>
+                 <input style="width: 100px;" type="text" v-model="item.total"/>                    
+              </td>              
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-card>
+  </v-container>   
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data(){
+    return {
+       isAdding : true,
+       date:'2023-06-13',
+       modal: false,
+       products: [
+          {
+            no : '1',
+            jenis_barang: '',
+            code_coil: '',
+            qty:'',
+            total_mtr:'',
+            harga:'',
+            total:'',
+          },          
+        ],
+    } 
+  },
+  methods: {
+      AddProduct(){
+         this.products.push(
+            {
+              no : '2',
+              jenis_barang: '',
+              code_coil: '',
+              qty:'',
+              total_mtr:'',
+              harga:'',
+              total:'',
+            }, 
+         )
+      }
+  }
 }
 </script>
